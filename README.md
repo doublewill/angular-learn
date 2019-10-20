8ed56145e846724a67fb1e49ac0c73ac9bf7e0c0
doublewill <xd8012@163.com>
2019-10-20 10:22:00 +0800

    before http

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # angular-learn
+
+#https://angular.cn
+
+Node.js version >= 10.9.0   node -v check
+
+angular install
+
+npm install -g @angular/cli
+
+create new projec：  ng new project-name
+
+use ng generate component  component-name or ng g c component-name to create a component
+
+use ng generate service service-name or ng g c component-name to create a service
+
+use  ng generate module app-routing --flat --module=app or ng g m app-routing --flat --module=app
+
+--flat 把这个文件放进了 src/app 中，而不是单独的目录中。
+--module=app 告诉 CLI 把它注册到 AppModule 的 imports 数组中。
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Hero } from '../hero';
+import { Location } from '@angular/common'
+import { HeroService } from '../hero.service'
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,9 +13,17 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id')
+    this.heroService.getHero(id).subscribe(hero=> this.hero = hero)
   }
 
+  ngOnInit() {
+    this.getHero()
+  }
 }
